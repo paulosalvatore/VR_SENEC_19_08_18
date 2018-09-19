b566f4056f5079d9e8a3ac92bcf425c36fd032ae
Paulo Salvatore <salvatoregames@gmail.com>
2018-09-19 16:55:33 -0300

    Initial commit

---
+++ Assets/Jogador.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jogador : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float velocidade = 2f;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+
+		rb.velocity = new Vector3(
+			h * velocidade,
+			rb.velocity.y,
+			v * velocidade
+		);
+	}
+}
